3ba0a401cdac84633bb7d85bc5a0f778096624e7
Damien Gasparina <d.gasparina@gmail.com>
2018-08-28 10:53:56 +0200

    refactorisation (it was time to become clean...)

---
+++ utils/utils.sh
@@ -0,0 +1,83 @@
+#! /bin/sh
+#
+# utils.sh
+# Copyright (C) 2018 gaspar_d </var/spool/mail/gaspar_d>
+#
+# Distributed under terms of the MIT license.
+#
+
+interface=eth0
+
+log() {
+	echo -n '$> ' 
+
+	echo "`echo $@ | tr '\r' '\n'`"
+}
+
+block_one_host() {
+	container=$1
+	ip=$2
+
+	docker-compose exec --privileged $container bash -c "tc qdisc add dev $interface root handle 1: prio" 2>&1 > /dev/null
+	docker-compose exec --privileged $container bash -c "tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1" 2>&1 > /dev/null
+	docker-compose exec --privileged $container bash -c "tc qdisc add dev $interface parent 1:1 handle 2: netem loss 100%" 2>&1 > /dev/null
+}
+
+remove_partition() {
+	for container in $@; do
+		docker-compose exec --privileged $container bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
+	done 
+}
+
+send_message() {
+	container=$1
+	shift 1
+	msg=$@
+
+	log "Sending messages to $container - $msg"
+	docker-compose exec  $container bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic test --sync --request-required-acks -1 --request-timeout-ms 10000"
+	echo
+}
+
+send_message_to_topic() {
+	container=$1
+	shift 1
+	topic=$1
+	shift 1
+	msg=$@
+
+	log "Sending messages to $container - $msg"
+	docker-compose exec  $container bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic $topic --sync --request-required-acks 1 --request-timeout-ms 10000"
+	echo
+}
+
+read_message() {
+	container=$1
+	log "Messages from $container:"
+	timeout 5 docker-compose exec $container kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning --timeout-ms 1000
+
+	if [ ! $? -eq 0 ]; then
+		log "Read unsuccessful" 
+	fi
+}
+
+get_state() {
+	container=$1
+	log "State for partition from $container"
+	docker-compose exec $container zookeeper-shell localhost:2181 get /brokers/topics/test/partitions/0/state | grep '{' | grep '}'
+}
+
+create_topic() {
+	container=$1
+	shift 1
+	topic=$1
+	shift 1
+	repl=$1
+	shift 1
+	min=$1
+
+	log "Creating topic with min.isr=$min and replication factor $repl"
+	log `docker-compose exec $container kafka-topics --zookeeper localhost:2181 --create --topic $topic --replication-factor $repl --partitions 1 --config min.insync.replicas=$min`
+}
+
+
