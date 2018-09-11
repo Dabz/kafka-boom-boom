8a0f9029dd50194204ced602fb8761d90b637af8
Florent Ramière <florent@ramiere.com>
2018-09-11 20:23:19 +0200

    run much faster with docker exec instead of slow docker-compose exec

--- utils/utils.sh
+++ utils/utils.sh
@@ -8,56 +8,80 @@
 
 interface=eth0
 
+##
+## A note about our usage of docker
+##
+## We should be using docker-compose everywhere, but it is slow.
+## We prefer relying directly on docker, thus the container to name translation with container_to_name
+##
+## Please note that we add -t as it is the default in docker-compose and we rely on tty
+
+
 log() {
 	echo -n '$> ' 
 
 	echo "`echo $@ | tr '\r' '\n'`"
 }
 
+container_to_name() {
+    container=$1
+    echo "${PWD##*/}_${container}_1"
+}
+
+container_to_ip() {
+    container=$1
+    name=$(container_to_name $container)
+    echo $(docker exec $name hostname -I)
+}
 
 block_host() {
-	container=$1
-	shift 1
-	docker-compose exec --privileged $container bash -c "tc qdisc add dev $interface root handle 1: prio" 2>&1 > /dev/null
-	docker-compose exec --privileged $container bash -c "tc qdisc add dev $interface parent 1:1 handle 2: netem loss 100%" 2>&1 > /dev/null
-	for ip in $@; do
-		docker-compose exec --privileged $container bash -c "tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1" 2>&1 > /dev/null
-	done
+    container=$1
+    name=$(container_to_name $container)
+    shift 1
+    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface root handle 1: prio" 2>&1
+    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:1 handle 2: netem loss 100%" 2>&1
+    for ip in $@; do
+        docker exec --privileged -t $name bash -c "tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1" 2>&1
+    done
 }
 
 remove_partition() {
 	for container in $@; do
-		docker-compose exec --privileged $container bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
+        name=$(container_to_name $container)
+		docker exec --privileged -t $name bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
 	done 
 }
 
 send_message() {
-	container=$1
+    container=$1
+    name=$(container_to_name $container)
 	shift 1
 	msg=$@
 
 	log "Sending messages to $container - $msg"
-	docker-compose exec  $container bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic test --sync --request-required-acks -1 --request-timeout-ms 10000"
+	docker exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic test --sync --request-required-acks -1 --request-timeout-ms 10000"
 	echo
 }
 
 send_message_to_topic() {
-	container=$1
+    container=$1
+    name=$(container_to_name $container)
 	shift 1
 	topic=$1
 	shift 1
 	msg=$@
 
 	log "Sending messages to $container - $msg"
-	docker-compose exec  $container bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic $topic --sync --request-required-acks 1 --request-timeout-ms 10000"
+	docker-compose exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic $topic --sync --request-required-acks 1 --request-timeout-ms 10000"
 	echo
 }
 
 read_messages() {
-	container=$1
+    container=$1
+    name=$(container_to_name $container)
 	number_of_messages_to_read=${2:-1}
-	log "Reading $number_of_messages_to_read from $container:"
-	docker-compose exec $container timeout 15 kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning --timeout-ms 1000 --max-messages $number_of_messages_to_read
+	log "Reading $number_of_messages_to_read messages from $container:"
+	docker exec -t $name timeout 15 kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning --timeout-ms 1000 --max-messages $number_of_messages_to_read
 
 	if [ ! $? -eq 0 ]; then
 		log "Read unsuccessful" 
@@ -65,14 +89,16 @@ read_messages() {
 }
 
 get_state() {
-	container=$1
+    container=$1
+    name=$(container_to_name $container)
 	log "State for partition from $container"
-	docker-compose exec $container zookeeper-shell localhost:2181 get /brokers/topics/test/partitions/0/state | grep '{' | grep '}'
+	docker exec -t $name zookeeper-shell localhost:2181 get /brokers/topics/test/partitions/0/state | grep '{' | grep '}'
 }
 
 zookeeper_mode() {
 	for container in $@; do
-	    mode=$(docker-compose exec $container bash -c "echo stat | nc localhost 2181 | grep Mode")
+        name=$(container_to_name $container)
+	    mode=$(docker exec -t $name bash -c "echo stat | nc localhost 2181 | grep Mode")
 
 	    if [ $? -eq 0 ]; then
     	    log "$container $mode"
@@ -83,7 +109,8 @@ zookeeper_mode() {
 }
 
 create_topic() {
-	container=$1
+    container=$1
+    name=$(container_to_name $container)
 	shift 1
 	topic=$1
 	shift 1
@@ -92,7 +119,7 @@ create_topic() {
 	min=$1
 
 	log "Creating topic with min.isr=$min and replication factor $repl"
-	log `docker-compose exec $container kafka-topics --zookeeper localhost:2181 --create --topic $topic --replica-assignment $(seq $repl | xargs | tr ' ' ':') --config min.insync.replicas=$min`
+	log `docker exec -t $name kafka-topics --zookeeper localhost:2181 --create --topic $topic --replica-assignment $(seq $repl | xargs | tr ' ' ':') --config min.insync.replicas=$min`
 }
 
 
