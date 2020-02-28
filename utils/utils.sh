f4e85a797b857e8c4d4e50d2b81e35f885d75249
Vincent de Saboulin <vdesabou@users.noreply.github.com>
2020-02-28 10:47:55 +0100

    Fixes for block_host(), timeout issues and typo fix for read_message function  (#2)
    * Fixed typo read_message -> read_messages
    
    * timeout-ms 10000 instead of 1000 for kafka-console-consumer (was failing with timeout)
    
    * added sleep after create_topic, otherwise get_state zookeeper is not displayed
    
    * tabs to space
    
    * Fixed block_host()
    
    credits to https://serverfault.com/a/906499

--- utils/utils.sh
+++ utils/utils.sh
@@ -18,9 +18,9 @@ interface=eth0
 
 
 log() {
-	echo -n '$> ' 
+    echo -n '$> '
 
-	echo "`echo $@ | tr '\r' '\n'`"
+    echo "`echo $@ | tr '\r' '\n'`"
 }
 
 container_to_name() {
@@ -38,70 +38,74 @@ block_host() {
     container=$1
     name=$(container_to_name $container)
     shift 1
+    # https://serverfault.com/a/906499
     docker exec --privileged -t $name bash -c "tc qdisc add dev $interface root handle 1: prio" 2>&1
-    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:1 handle 2: netem loss 100%" 2>&1
     for ip in $@; do
-        docker exec --privileged -t $name bash -c "tc filter add dev $interface parent 1:0 protocol ip prio 1 u32 match ip dst $ip flowid 2:1" 2>&1
+        docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol ip parent 1: prio 1 u32 match ip dst $ip flowid 1:1" 2>&1
     done
+    docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol all parent 1: prio 2 u32 match ip dst 0.0.0.0/0 flowid 1:2" 2>&1
+    docker exec --privileged -t $name bash -c "tc filter add dev $interface protocol all parent 1: prio 2 u32 match ip protocol 1 0xff flowid 1:2" 2>&1
+    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:1 handle 10: netem loss 100%" 2>&1
+    docker exec --privileged -t $name bash -c "tc qdisc add dev $interface parent 1:2 handle 20: sfq" 2>&1
 }
 
 remove_partition() {
-	for container in $@; do
+    for container in $@; do
         name=$(container_to_name $container)
-		docker exec --privileged -t $name bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
-	done 
+        docker exec --privileged -t $name bash -c "tc qdisc del dev $interface root" 2>&1 > /dev/null
+    done
 }
 
 send_message() {
     container=$1
     name=$(container_to_name $container)
-	shift 1
-	msg=$@
+    shift 1
+    msg=$@
 
-	log "Sending messages to $container - $msg"
-	docker exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic test --sync --request-required-acks -1 --request-timeout-ms 10000"
-	echo
+    log "Sending messages to $container - $msg"
+    docker exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic test --sync --request-required-acks -1 --request-timeout-ms 10000"
+    echo
 }
 
 send_message_to_topic() {
     container=$1
     name=$(container_to_name $container)
-	shift 1
-	topic=$1
-	shift 1
-	msg=$@
+    shift 1
+    topic=$1
+    shift 1
+    msg=$@
 
-	log "Sending messages to $container - $msg"
-	docker-compose exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic $topic --sync --request-required-acks 1 --request-timeout-ms 10000"
-	echo
+    log "Sending messages to $container - $msg"
+    docker-compose exec -t $name bash -c "echo $msg | kafka-console-producer --broker-list localhost:9092 --topic $topic --sync --request-required-acks 1 --request-timeout-ms 10000"
+    echo
 }
 
 read_messages() {
     container=$1
     name=$(container_to_name $container)
-	number_of_messages_to_read=${2:-1}
-	log "Reading $number_of_messages_to_read messages from $container:"
-	docker exec -t $name timeout 15 kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning --timeout-ms 1000 --max-messages $number_of_messages_to_read
+    number_of_messages_to_read=${2:-1}
+    log "Reading $number_of_messages_to_read messages from $container:"
+    docker exec -t $name timeout 15 kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning --timeout-ms 10000 --max-messages $number_of_messages_to_read
 
-	if [ ! $? -eq 0 ]; then
-		log "Read unsuccessful" 
-	fi
+    if [ ! $? -eq 0 ]; then
+        log "Read unsuccessful"
+    fi
 }
 
 get_state() {
     container=$1
     name=$(container_to_name $container)
-	log "State for partition from $container"
-	docker exec -t $name zookeeper-shell localhost:2181 get /brokers/topics/test/partitions/0/state | grep '{' | grep '}'
+    log "State for partition from $container"
+    docker exec -t $name zookeeper-shell localhost:2181 get /brokers/topics/test/partitions/0/state | grep '{' | grep '}'
 }
 
 zookeeper_mode() {
-	for container in $@; do
+    for container in $@; do
         name=$(container_to_name $container)
-	    mode=$(docker exec -t $name bash -c "echo stat | nc localhost 2181 | grep Mode")
+        mode=$(docker exec -t $name bash -c "echo stat | nc localhost 2181 | grep Mode")
 
-	    if [ $? -eq 0 ]; then
-    	    log "$container $mode"
+        if [ $? -eq 0 ]; then
+            log "$container $mode"
         else
             log "$container has no mode"
         fi
@@ -111,15 +115,15 @@ zookeeper_mode() {
 create_topic() {
     container=$1
     name=$(container_to_name $container)
-	shift 1
-	topic=$1
-	shift 1
-	repl=$1
-	shift 1
-	min=$1
+    shift 1
+    topic=$1
+    shift 1
+    repl=$1
+    shift 1
+    min=$1
 
-	log "Creating topic with min.isr=$min and replication factor $repl"
-	log `docker exec -t $name kafka-topics --zookeeper localhost:2181 --create --topic $topic --replica-assignment $(seq $repl | xargs | tr ' ' ':') --config min.insync.replicas=$min`
+    log "Creating topic with min.isr=$min and replication factor $repl"
+    log `docker exec -t $name kafka-topics --zookeeper localhost:2181 --create --topic $topic --replica-assignment $(seq $repl | xargs | tr ' ' ':') --config min.insync.replicas=$min`
 }
 
 
